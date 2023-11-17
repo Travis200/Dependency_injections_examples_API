052679ac45637d591f731d9f28708c4ad0b5b8a4
t.higgins <t.higgins@spglobal.com>
2023-11-17 11:37:36 +0000

    Created (empty) API routes.

--- Dependency_Injection_Examples_API/Program.cs
+++ Dependency_Injection_Examples_API/Program.cs
@@ -3,4 +3,28 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
+app.MapGet("/coffee/constructor_injection", (string drinkFlavour) => "") {
+
+}
+
+app.MapGet("/coffee/method_injection", (string drinkFlavour) => "") {
+
+}
+
+app.MapGet("/coffee/property_injection", (string drinkFlavour) => "") {
+
+}
+
+app.MapGet("/milkshake/constructor_injection", (string drinkFlavour) => "") {
+
+}
+
+app.MapGet("/milkshake/method_injection", (string drinkFlavour) => "") {
+
+}
+
+app.MapGet("/milkshake/property_injection", (string drinkFlavour) => "") {
+
+}
+
 app.Run();
